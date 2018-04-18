89d97108cafed712a28f1fb536162adec024a1d1
靳玉亮 <leo@air.local>
2018-04-18 11:58:10 -0500

    add command to make the jar file for a specific model only

--- NearestNeighbors/src/com/javferna/packtpub/mastering/knn/main/DataUtil.java
+++ NearestNeighbors/src/com/javferna/packtpub/mastering/knn/main/DataUtil.java
@@ -2,8 +2,10 @@ package com.javferna.packtpub.mastering.knn.main;
 
 public class DataUtil {
     //File path config for jar running
-    public static String BankPath = "NearestNeighbors/bank.data";
-    public static String TestPath = "NearestNeighbors/bank.test";
+    //When to build the jar of just this model, try to open this model as an complete project(seperate from the rest models and the parent
+    // project) and build the artifact.
+    public static String BankPath = "bank.data";
+    public static String TestPath = "bank.test";
     //File path config for local running
 //    public static String BankPath = "NearestNeighbors/data/bank.data";
 //    public static String TestPath = "NearestNeighbors/data/bank.test";
