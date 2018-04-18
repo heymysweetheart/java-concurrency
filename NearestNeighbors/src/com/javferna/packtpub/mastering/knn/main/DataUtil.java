8671d43b7f1d806b8fec36137bab67c40916a0ad
靳玉亮 <leo@air.local>
2018-04-18 11:59:59 -0500

    update documentation to make jar file

--- NearestNeighbors/src/com/javferna/packtpub/mastering/knn/main/DataUtil.java
+++ NearestNeighbors/src/com/javferna/packtpub/mastering/knn/main/DataUtil.java
@@ -4,6 +4,7 @@ public class DataUtil {
     //File path config for jar running
     //When to build the jar of just this model, try to open this model as an complete project(seperate from the rest models and the parent
     // project) and build the artifact.
+    //Remember to mark the data folder as the Resources root, and data foder and src folder will disappear in the jar folder.
     public static String BankPath = "bank.data";
     public static String TestPath = "bank.test";
     //File path config for local running
