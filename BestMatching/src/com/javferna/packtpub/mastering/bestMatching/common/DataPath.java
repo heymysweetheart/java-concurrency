a5b6d633028db85672fff901fd6cc69249102ab3
靳玉亮 <leo@air.local>
2018-04-18 15:49:29 -0500

    modify file read bugs

--- BestMatching/src/com/javferna/packtpub/mastering/bestMatching/common/DataPath.java
+++ BestMatching/src/com/javferna/packtpub/mastering/bestMatching/common/DataPath.java
@@ -1,6 +1,9 @@
 package com.javferna.packtpub.mastering.bestMatching.common;
 
 public class DataPath {
+    //We could use this one path from the data folder, but we have to mark data folder as the Resources root.
     public static String PATH = "UK Advanced Cryptics Dictionary.txt";
-//    public static String PATH = "BestMatching/data/UK Advanced Cryptics Dictionary.txt";
+
+    //We could also use the bellow one and don't need to mark resources folder as the Resources root.
+//    public static String PATH = "resources/UK Advanced Cryptics Dictionary.txt";
 }
